32d27a161208268a1133d5257161799449547a1e
TommyMa99 <ma.jianq@northeasdtern.edu>
2021-10-22 01:14:15 -0700

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+
+/*
+task.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error', error)
+})*/
